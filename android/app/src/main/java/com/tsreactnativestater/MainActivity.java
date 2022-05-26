9a47812409f48c97caec07774ad37d03afe1db0e
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-05-26 14:52:26 +0530

    @supersami/rn-foreground-service package properly installed for handling foreground services

--- android/app/src/main/java/com/tsreactnativestater/MainActivity.java
+++ android/app/src/main/java/com/tsreactnativestater/MainActivity.java
@@ -4,6 +4,13 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+// Add the following imports for foreground services
+import android.content.Intent;
+import android.util.Log;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -42,6 +49,45 @@ public class MainActivity extends ReactActivity {
       // If you opted-in for the New Architecture, we enable the Fabric Renderer.
       reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
       return reactRootView;
+    }
+  }
+
+  // Add following code for foreground services
+  public boolean isOnNewIntent = false;
+
+  @Override
+  public void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+    isOnNewIntent = true;
+    ForegroundEmitter();
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    if(isOnNewIntent == true){}else {
+        ForegroundEmitter();
+    }
+  }
+
+  public  void  ForegroundEmitter(){
+    // this method is to send back data from java to javascript so one can easily
+    // know which button from notification or the notification button is clicked
+    String  main = getIntent().getStringExtra("mainOnPress");
+    String  btn = getIntent().getStringExtra("buttonOnPress");
+    WritableMap  map = Arguments.createMap();
+    if (main != null) {
+        map.putString("main", main);
+    }
+    if (btn != null) {
+        map.putString("button", btn);
+    }
+    try {
+        getReactInstanceManager().getCurrentReactContext()
+        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+        .emit("notificationClickHandle", map);
+    } catch (Exception  e) {
+    Log.e("SuperLog", "Caught Exception: " + e.getMessage());
     }
   }
 }
--- index.js
+++ index.js
@@ -6,5 +6,15 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
+import ReactNativeForegroundService from '@supersami/rn-foreground-service';
+
+// Register the service
+ReactNativeForegroundService.register();
+
+ReactNativeForegroundService.start({
+  id: 144,
+  title: 'Foreground Service',
+  message: 'you are online!',
+});
 
 AppRegistry.registerComponent(appName, () => App);
