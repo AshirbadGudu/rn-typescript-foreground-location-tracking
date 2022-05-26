c4ae2bb62e2b4f1e0a680a676f578ff399262f93
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-05-26 16:12:55 +0530

    location tracking in background complete

--- src/screens/private/Home.tsx
+++ src/screens/private/Home.tsx
@@ -1,11 +1,55 @@
-import {Heading} from 'native-base';
+import {Box, Button, Heading} from 'native-base';
 import React from 'react';
-import {GOOGLE_API_KEY} from 'react-native-dotenv';
+import {Alert, PermissionsAndroid, Platform} from 'react-native';
+import RNLocation from 'react-native-location';
+import ReactNativeForegroundService from '@supersami/rn-foreground-service';
+
+RNLocation.configure({
+  distanceFilter: 5.0,
+});
+
+const askForLocationPermission = async () => {
+  try {
+    const isGranted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_BACKGROUND_LOCATION,
+    );
+
+    if (isGranted !== PermissionsAndroid.RESULTS.GRANTED)
+      return Alert.alert(
+        'Location Permission Denied',
+        'Please visit setting page to allow location permission',
+      );
+    RNLocation.requestPermission({
+      android: {
+        detail: 'coarse',
+      },
+    }).then(granted => {
+      if (granted) {
+        RNLocation.getLatestLocation()
+          .then(location => {
+            console.log(location?.latitude, location?.longitude);
+          })
+          .catch(e => {
+            console.log(e);
+          });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+ReactNativeForegroundService.add_task(() => askForLocationPermission(), {
+  delay: 1000,
+  onLoop: true,
+  taskId: 'taskid',
+  onError: (e: any) => console.log(`Error logging:`, e),
+});
 
 export default function Home() {
   return (
-    <>
-      <Heading>{GOOGLE_API_KEY}</Heading>
-    </>
+    <Box p="4">
+      <Button onPress={askForLocationPermission}>Ask For Permission</Button>
+    </Box>
   );
 }
